befc33412f646684da85606cc8f29ca9b2d879c7
Jayson <jaysontobias101@gmail.com>
2021-03-12 03:59:44 +0800

    update

--- src/baseService.js
+++ src/baseService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 class BaseService {
   constructor(moduleUrl) {
-    this.URL = "http://localhost:14000" + "/" + moduleUrl;
+    this.URL = "http://165.232.166.12:14000" + "/" + moduleUrl;
     this.axiosInstance = axios.create({
-      baseURL: "http://localhost:14000" + "/" + moduleUrl
+      baseURL: "http://165.232.166.12:14000" + "/" + moduleUrl
     })
     
     // this.axiosInstance.interceptors.request.use(function (config) {
