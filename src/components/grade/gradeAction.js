7a900f5eb0f216f03648fb676855ae30144c1792
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-08 01:12:57 +0800

    Update gradeAction.js

--- src/components/grade/gradeAction.js
+++ src/components/grade/gradeAction.js
@@ -5,6 +5,8 @@ import advisoryService from '../grade/advisoryService';
 import auditTrailService from '../auditTrail/auditTrailService';
 import { Button} from 'antd';
 import moment from 'moment';
+import auditTrailService from '../auditTrail/auditTrailService';
+
 const Grade1Action = (initial = { searchRequest: {} }) => {
   let [studentAdvisor, setStudentAdvisor] = useState({});
   let [selectedUserGrade, setSelectedUserGrade] = useState([]);
@@ -383,9 +385,15 @@ const Grade1Action = (initial = { searchRequest: {} }) => {
       grade.subjects[5].subjectGrade = newValuesGrade;
     }
 
-
-
     let updateResult = await gradesService.update(grade);
+    //add record  to audit trail
+    let loggedDate =  new Date().toISOString();
+    let userDetails = {
+      user: JSON.parse(sessionStorage.user),
+      activity: 'Teacher has updated student grades',
+      date: loggedDate
+    };
+    await auditTrailService.add(details);
     window.location.reload();
   };
 
