71369af9e34f228b813e9d0a2bdbc31d37d50ace
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-12 00:12:48 +0800

    Update user.js

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -61,7 +61,7 @@ const EmployeePage = () => {
               onClose={()=> { setShowUserVisible(false)}}
               bodyStyle={{ paddingBottom: 80 }}
           >
-             <UserForm add={addUser} update={editUser} selectedUser={selectedUser} selectedParent={selectedParent} role='Admin'/>
+             <UserForm add={addUser} update={editUser} selectedUser={selectedUser} selectedParent={selectedParent} role='Admin' isNewData=false/>
           </Drawer>
     
   </Card>
