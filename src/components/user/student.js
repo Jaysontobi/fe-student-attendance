36e6f8b367b4b36289ee16afffbf2212a746ab20
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-12 23:32:48 +0800

    Update student.js

--- src/components/user/student.js
+++ src/components/user/student.js
@@ -27,24 +27,24 @@ const EmployeePage = ({isAdvisory}) => {
     today
   } = TimeKeepingAction({});
   
-  const filterStudents = async list => {
+  const filterStudents = list => {
 
      if (!list || list.length === 0) return [];
 	  return list;
 	  
-     if (isAdvisory) {
-       let user = JSON.parse(sessionStorage.user);
-       const userData = {
-       	 firstName: user.firstName,
-	 middleName: user.middleName,
-	 lastName: user.lastName
-       };
-       let advisory = await AdditionalService.getAdvisory(userData);
-       let newList = list.filter(student => student.gradeLevel === advisory.gradeLevel);
-       return newList;
-     } else {
-     	return list;
-     };
+//      if (isAdvisory) {
+//        let user = JSON.parse(sessionStorage.user);
+//        const userData = {
+//        	 firstName: user.firstName,
+// 	 middleName: user.middleName,
+// 	 lastName: user.lastName
+//        };
+//        let advisory = await AdditionalService.getAdvisory(userData);
+//        let newList = list.filter(student => student.gradeLevel === advisory.gradeLevel);
+//        return newList;
+//      } else {
+//      	return list;
+//      };
   };	
 
   return (
