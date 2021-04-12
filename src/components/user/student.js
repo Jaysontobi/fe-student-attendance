6b6d59943d949a647e642c8fa336f29e32bf6049
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-12 15:44:43 +0800

    Update student.js

--- src/components/user/student.js
+++ src/components/user/student.js
@@ -8,7 +8,7 @@ import TimeKeepingAction from '../timeKeeping/timeKeepingAction'
 
 import UserTable from './userTable'
 
-const EmployeePage = () => {
+const EmployeePage = ({isAdvisory = false}) => {
 
 
   let {
