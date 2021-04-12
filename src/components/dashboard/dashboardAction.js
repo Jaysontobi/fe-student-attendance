f82db4e17079300a45436e3865b540f1f79b7517
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-12 17:59:42 +0800

    Update dashboardAction.js

--- src/components/dashboard/dashboardAction.js
+++ src/components/dashboard/dashboardAction.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import userService from '../user/userService';
-const UserAction = ({initial = { searchRequest: {} }, isAdvisory}) => {
+const UserAction = (initial = { searchRequest: {} }) => {
   let [userDetails, setUserDetails] = useState( 
       {numberOfStudents : 0, 
       numberOfTeachers: 0,
@@ -10,7 +10,7 @@ const UserAction = ({initial = { searchRequest: {} }, isAdvisory}) => {
   const getListOfUsers = async () => {
     let response = await userService.findAllUser();
     let parent = response.data.filter(user => user.role === "Parent");
-    let student = response.data.filter(user => user.role === "Student" && user.gender === "Male");
+    let student = response.data.filter(user => user.role === "Student");
     let teacher = response.data.filter(user => user.role === "Teacher");
     setUserDetails({
         numberOfParents: parent.length,
