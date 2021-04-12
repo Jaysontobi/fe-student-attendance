979aabf3866c000986703778295e095973f1c202
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-12 22:47:01 +0800

    Update additionalService.js

--- src/components/user/additionalService.js
+++ src/components/user/additionalService.js
@@ -4,9 +4,18 @@ class AdditionalService extends BaseService {
     super('');
   };
 
-  getGenIdNumber = (role) => {
+  getGenIdNumber = role => {
      return this.axiosInstance.get('/generate-user-id/' + role);
   };
+
+  getAdvisoryStudents = level => {
+    return this.axiosInstance.get('/advisory-students/' + level);
+  };
+
+  getAdvisory = teacher => {
+    return this.axiosInstance.post('/teacher-advisory', teacher);
+  };
+
 }
 
 export default new AdditionalService();
