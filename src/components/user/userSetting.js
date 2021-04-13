a05bc26a42ad39caa30095615e37d4e8d80daa4c
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-13 18:38:36 +0800

    Update userSetting.js

--- src/components/user/userSetting.js
+++ src/components/user/userSetting.js
@@ -4,83 +4,32 @@ import { Drawer, Card, Button, Typography, Icon, Row, Col, Input, Space, Alert }
 import UserAction from './userAction';
 
 const Settings = () => {
-//   let password = '';
-//   let newPassword = '';
-//   let confirmPassword = '';
-//   let error = false;
-//   let success = false;
+  let password = '';
+  let newPassword = '';
+  let confirmPassword = '';
+  let error = false;
+  let success = false;
   
-//   const changePassword = () => {
-//     console.log('________');
-//     if (password === '' || newPassword === '' || confirmPassword === '') return error = true;
-//     return success = true;
-//   };
+  const changePassword = () => {
+    console.log('________');
+    if (password === '' || newPassword === '' || confirmPassword === '') return error = true;
+    return success = true;
+  };
   
   
   return (
-   <React.Fragment>
-    <Card className="h-82 p-70" style={{textAlign: 'center'}}>
+    <Card className="h-82 p-70">
      <Row className="mt-15">
-       <Col lg={{ span: 6, offset: 9 }}>
-         <Typography.Title level={3} className="ml-15">Settings</Typography.Title>
+       <Col lg={{ span: 14 }}>
+         <Typography.Title level={3} className="ml-15">Setting</Typography.Title>
        </Col>
      </Row>
-//      <form onSubmit={() => changePassword()}>
-//        <Row className="mt-15">
-//         <Col lg={{ span: 6, offset: 9 }}>
-//           <Input placeholder="Old Password" onChange={(e) => password = e.target.value}/>
-//         </Col>
-//       </Row>
-//       <Row className="mt-15">
-//         <Col lg={{ span: 6, offset: 9 }}>
-//           <Input placeholder="New Password" onChange={(e) => newPassword = e.target.value}/>
-//         </Col>
-//       </Row>
-//       <Row className="mt-15">
-//         <Col lg={{ span: 6, offset: 9 }}>
-//           <Input placeholder="Confirm Password" onChange={(e) => confirmPassword = e.target.value}/>
-//         </Col>
-//       </Row>
-//       <Row style={{marginTop: '15px'}}>
-//        <Col lg={{ span: 6, offset: 9 }}>
-//         <Space align="center">
-//           <Button  className="addUser btn-add" onClick={()=> changePassword()}>
-//             Change Password
-//           </Button>
-//         </Space>
-//        </Col>
-//       </Row>
-//      </form>
-//      { error ? (
-//       <React.Fragment>
-//          <Row className="mt-15">
-//            <Col lg={{ span: 6, offset: 9 }}>
-//              <Alert
-//                message="Error"
-//                description="Please fill all fields."
-//                type="error"
-//                showIcon
-//             />
-//           </Col>
-//         </Row>
-//       </React.Fragment>
-//       ) : ("")}
-//         { success ? (
-//        <React.Fragment>
-//          <Row className="mt-15">
-//            <Col lg={{ span: 6, offset: 9 }}>
-//              <Alert
-//                message="Success"
-//                description="Password changed successfully."
-//                type="success"
-//                showIcon
-//             />
-//           </Col>
-//         </Row>
-//       </React.Fragment>
-//       ) : ("")}
-    </Card>
-   </React.Fragment>
+     <Row>
+       <Col lg={{ span: "24" }}>
+          dgdfgdfgdfgdfgd
+       </Col>
+      </Row>
+    </Card>     
   );
 }
 
