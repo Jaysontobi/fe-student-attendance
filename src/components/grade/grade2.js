c7c2080932b93e9a00d5616ec58ebbb9d67cad35
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-16 00:40:40 +0800

    Update grade2.js

--- src/components/grade/grade2.js
+++ src/components/grade/grade2.js
@@ -117,7 +117,7 @@ const GradePage = () => {
               onClose={()=> hideAdvisor()}
               bodyStyle={{ paddingBottom: 80 }}
           >
-             <AdvisoryForm selectedTeacherAssgined={selectedAdvisoryAssignedGrade2} selectedTeacher={availableAdvisors} add={addAdvisor} gradeLevel={"2"}/>
+             <AdvisoryForm selectedTeacherAssgined={selectedAdvisoryAssignedGrade2} selectedTeacher={selectedTeacher} add={addAdvisor} gradeLevel={"2"}/>
           </Drawer>
     </Spin>
   </Card>
