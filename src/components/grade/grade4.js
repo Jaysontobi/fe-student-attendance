54dc32feaf79b3b0428eeffb0ae9ce351a1e5fdf
palanas-jeffrey <44762827+palanas-jeffrey@users.noreply.github.com>
2021-04-16 01:02:34 +0800

    Update grade4.js

--- src/components/grade/grade4.js
+++ src/components/grade/grade4.js
@@ -44,7 +44,8 @@ const GradePage = () => {
     showAdvisor,
     addAdvisor,
     selectedAdvisoryAssignedGrade4,
-    selectedAdvisoryAssgined
+    selectedAdvisoryAssgined,
+    availableAdvisors
   } = AdvisorAction({});
 
 
@@ -115,7 +116,7 @@ const GradePage = () => {
               onClose={()=> hideAdvisor()}
               bodyStyle={{ paddingBottom: 80 }}
           >
-             <AdvisoryForm selectedTeacherAssgined={selectedAdvisoryAssignedGrade4} selectedTeacher={selectedTeacher} add={addAdvisor} gradeLevel={"4"}/>
+             <AdvisoryForm selectedTeacherAssgined={selectedAdvisoryAssignedGrade4} selectedTeacher={availableAdvisors} add={addAdvisor} gradeLevel={"4"}/>
           </Drawer>
           </Spin>
   </Card>
